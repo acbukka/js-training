ebfaa25d003bd2c671b5d3a4d3703df5ac437500
acbukka <thesweylis@gmail.com>
2021-12-15 22:32:28 +0700

    add new files

---
+++ js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// const MarkMass = 95;
+// const MarkHeight = 1.88;
+// const JohnMass = 85;
+// const JohnHeight = 1.76;
+
+// const MarkBMI = MarkMass / (MarkHeight ** 2);
+// const JohnBMI = JohnMass / (JohnHeight ** 2);
+// const MarkHigherBMI = MarkBMI > JohnBMI;
+// console.log(MarkBMI, JohnBMI);
+// if (MarkBMI > JohnBMI) {
+//   console.log('Mark has higher BMI');
+// } else {
+//   console.log('Mark has lower BMI');
+// }
+// console.log(MarkHigherBMI);
+
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
+
+console.log('String with \n\ muliple \n\ lines');
+
+console.log(`String with
+multiple
+lines`);
+
+
+
+
+
+
+
